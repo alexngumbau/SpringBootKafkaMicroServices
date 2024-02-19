87d986aa13bfe5502af5354f9f0f9bb82237a7eb
Alex Mutinda Ngumbau <ALEX.NGUMBAU@equitybank.co.ke>
2024-02-19 09:34:44 +0300

    Created Kafka consumer in stock service

---
+++ stock-service/stock-service/src/main/java/com/alex/stockservice/kafka/OrderConsumer.java
@@ -0,0 +1,21 @@
+package com.alex.stockservice.kafka;
+
+import com.alex.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER   = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event) {
+        LOGGER.info(String.format("Order event received in stock service => %s", event.toString()));
+
+        // save the event data into the database
+
+    }
+}
