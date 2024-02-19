8e9beee6dbeece1fe7059f143e4785d89ecb545a
Alex Mutinda Ngumbau <ALEX.NGUMBAU@equitybank.co.ke>
2024-02-19 08:41:01 +0300

    First Commit

---
+++ stock-service/stock-service/src/main/java/com/alex/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alex.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
---
+++ stock-service/stock-service/src/test/java/com/alex/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
